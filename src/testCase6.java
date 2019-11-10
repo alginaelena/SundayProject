aaa438d69a26c5f9aa15c9310ed0d05f1a9f37e6
elenagrash <12345>
2019-11-10 12:10:56 -0600

    pushing from terminal

--- src/testCase6.java
+++ src/testCase6.java
@@ -2,5 +2,9 @@ public class testCase6 {
     public static void main(String[] args) {
 
         System.out.println("Elena tester case 6");
+        System.out.println("Elena tester case 6");
+        System.out.println("Elena tester case 6");
+        System.out.println("Elena tester case 6");
+
     }
 }
