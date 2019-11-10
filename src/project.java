76e3e1f133b214e7b67b53f36bf53e3d371b4483
alginaelena <56318325+alginaelena@users.noreply.github.com>
2019-11-10 11:40:26 -0600

    Update project.java

--- src/project.java
+++ src/project.java
@@ -1,6 +1,13 @@
 public class project {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("change some");
+        System.out.println("change some");
+        System.out.println("change some");
+        System.out.println("change some");
+        System.out.println("change some");
+        System.out.println("change some");
+        System.out.println("change some");
     }
 
 
